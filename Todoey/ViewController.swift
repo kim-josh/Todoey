f73941ef1d9427a51fbdbbcbcf151c74200728ff
Josh Kim <josh94kim@berkeley.edu>
2018-11-13 12:51:56 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Josh Kim on 11/13/18.
+//  Copyright © 2018 Josh Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
